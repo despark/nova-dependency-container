dcf151186ce2276b1bfe14a5fedf1ab6a6d9cadc
Anton Geshev <ageshev@despark.com>
2019-06-25 15:27:02 +0300

    FormField supporting object comparison

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,97 +1,117 @@
 <template>
-	<div v-if="dependenciesSatisfied">
-		<div v-for="childField in field.fields">
-			<component
-				:is="'form-' + childField.component"
-                :errors="errors"
-				:resource-id="resourceId"
-				:resource-name="resourceName"
-				:field="childField"
-				:ref="'field-' + childField.attribute"
-			/>
-		</div>
-	</div>
+    <div v-if="dependenciesSatisfied">
+        <div v-for="childField in field.fields">
+            <component
+                    :is="'form-' + childField.component"
+                    :errors="errors"
+                    :resource-id="resourceId"
+                    :resource-name="resourceName"
+                    :field="childField"
+                    :ref="'field-' + childField.attribute"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
-	import {FormField, HandlesValidationErrors} from 'laravel-nova'
+    import {FormField, HandlesValidationErrors} from 'laravel-nova'
 
-	export default {
-		mixins: [FormField, HandlesValidationErrors],
+    export default {
+        mixins: [FormField, HandlesValidationErrors],
 
-		props: ['resourceName', 'resourceId', 'field'],
+        props: ['resourceName', 'resourceId', 'field'],
 
-		mounted() {
-			this.registerDependencyWatchers(this.$root)
-			this.updateDependencyStatus()
-		},
+        mounted() {
+            this.registerDependencyWatchers(this.$root)
+            this.updateDependencyStatus()
+        },
 
-		data() {
-			return {
-				dependencyValues: {},
-				dependenciesSatisfied: false,
-			}
-		},
+        data() {
+            return {
+                dependencyValues: {},
+                dependenciesSatisfied: false,
+            }
+        },
 
-		methods: {
+        methods: {
 
-			registerDependencyWatchers(root) {
-				root.$children.forEach(component => {
-					if (this.componentIsDependency(component)) {
+            registerDependencyWatchers(root) {
+                root.$children.forEach(component => {
+                    if (this.componentIsDependency(component)) {
 
-						component.$watch('value', (value) => {
-							this.dependencyValues[component.field.attribute] = value;
-							this.updateDependencyStatus()
-						}, {immediate: true})
+                        component.$watch('value', (value) => {
+                            this.dependencyValues[component.field.attribute] = value;
+                            this.updateDependencyStatus()
+                        }, {immediate: true})
 
-						this.dependencyValues[component.field.attribute] = component.field.value;
+                        this.dependencyValues[component.field.attribute] = component.field.value;
 
-					}
+                    }
 
-					this.registerDependencyWatchers(component)
-				})
-			},
+                    this.registerDependencyWatchers(component)
+                })
+            },
 
-			componentIsDependency(component) {
-				if(component.field === undefined) {
-					return false;
-				}
+            componentIsDependency(component) {
+                if (component.field === undefined) {
+                    return false;
+                }
 
-				for (let dependency of this.field.dependencies) {
-					if(component.field.attribute === dependency.field) {
-						return true;
-					}
-				}
+                for (let dependency of this.field.dependencies) {
+                    if (component.field.attribute === dependency.field) {
+                        return true;
+                    }
+                }
 
-				return false;
-			},
+                return false;
+            },
 
-			updateDependencyStatus() {
-				for (let dependency of this.field.dependencies) {
-					if(dependency.hasOwnProperty('notEmpty') && ! this.dependencyValues[dependency.field]) {
-						this.dependenciesSatisfied = false;
-						return;
-					}
+            updateDependencyStatus() {
+                for (let dependency of this.field.dependencies) {
+                    if (dependency.hasOwnProperty('notEmpty') && !this.dependencyValues[dependency.field]) {
+                        this.dependenciesSatisfied = false;
+                        return;
+                    }
 
-					if(dependency.hasOwnProperty('value') && this.dependencyValues[dependency.field] !== dependency.value) {
-						this.dependenciesSatisfied = false;
-						return;
-					}
-				}
+                    if (dependency.hasOwnProperty('value') && typeof this.dependencyValues[dependency.field] === 'object' && typeof dependency.value === 'object' && this.dependencyValues[dependency.field] && this.dependencyValues[dependency.field].length) {
+                        for (let i = 0; i < dependency.value.length; i++) {
+                            let firstObject = dependency.value[i];
 
-				this.dependenciesSatisfied = true;
-			},
+                            for (let j = 0; j < this.dependencyValues[dependency.field].length; j++) {
+                                let secondObject = this.dependencyValues[dependency.field][j];
 
-			fill(formData) {
-				if(this.dependenciesSatisfied) {
-					_.each(this.field.fields, field => {
-						if (field.fill) {
-							field.fill(formData)
-						}
-					})
-				}
-			}
+                                for (let property in firstObject) {
+                                    if (firstObject.hasOwnProperty(property) && secondObject.hasOwnProperty(property) && firstObject[property] === secondObject[property]) {
+                                        this.dependenciesSatisfied = true;
+                                        return;
+                                    } else {
+                                        this.dependenciesSatisfied = false;
+                                    }
+                                }
+                            }
+                        }
 
-		}
-	}
+                        return;
+                    }
+
+                    if (dependency.hasOwnProperty('value') && this.dependencyValues[dependency.field] !== dependency.value) {
+                        this.dependenciesSatisfied = false;
+                        return;
+                    }
+                }
+
+                this.dependenciesSatisfied = true;
+            },
+
+            fill(formData) {
+                if (this.dependenciesSatisfied) {
+                    _.each(this.field.fields, field => {
+                        if (field.fill) {
+                            field.fill(formData)
+                        }
+                    })
+                }
+            }
+        }
+    }
 </script>
